9ec79dcbb3f648665142af77efccb9a3f7c9c171
Rumia <147132163+Rumiia04@users.noreply.github.com>
2024-05-06 23:49:11 +0300

    Add files via upload

---
+++ C++/Sets/Sets/Sets.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    set<int> group;
+    int num;
+
+    cout << "Введите число: ";
+    cin >> num;
+
+    while ( cin >> num) {
+        if (group.find(num) == group.end()) {
+            cout << "NO" << endl;
+        }
+        else {
+            cout << "YES" << endl;
+        }
+        group.insert(num);
+    }
+    return 0;
+}
---
+++ C++/WorkInFile/WorkInFile.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Student {
+    string firstName;
+    string lastName;
+    int mathGrade;
+    int physicsGrade;
+    int informaticsGrade;
+    double averageGrade;
+};
+
+bool compareStudents(const Student& s1, const Student& s2) {
+    return s1.averageGrade > s2.averageGrade;
+}
+
+int main() {
+    string line = "DataRating.txt";
+
+    ofstream fout;
+
+    fout.open(line);
+
+    if (!fout.is_open())
+    {
+        cout << "Error opening file!\n";
+    }
+    else
+    {
+        fout << "Markov Valeriy 5 5 5\nSergey Petrov 1 1 1\nPetrov Petr 3 3 3";
+    }
+    fout.close();
+
+    ifstream fin("DataRating.txt");
+    if (!fin.is_open()) {
+        cout << "Error opening file!\n";
+        return 1;
+    }
+
+    vector<Student> students;
+    string firstName, lastName;
+    int mathGrade, physicsGrade, informaticsGrade;
+
+    while (fin >> firstName >> lastName >> mathGrade >> physicsGrade >> informaticsGrade) {
+        Student student;
+        student.firstName = firstName;
+        student.lastName = lastName;
+        student.mathGrade = mathGrade;
+        student.physicsGrade = physicsGrade;
+        student.informaticsGrade = informaticsGrade;
+        student.averageGrade = (mathGrade + physicsGrade + informaticsGrade) / 3.0;
+        students.push_back(student);
+    }
+
+    fin.close();
+
+    sort(students.begin(), students.end(), compareStudents);
+
+    for (const auto& student : students) {
+        cout << student.firstName << " " << student.lastName << endl;
+    }
+
+    return 0;
+}
